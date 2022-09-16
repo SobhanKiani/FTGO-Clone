1db78a105629dd121c1021b2ee57d3c4db9ce308
SobhanKiani <45046992+SobhanKiani@users.noreply.github.com>
2022-09-17 00:21:06 +0430

    SFC-14 update restaurant mutation added

---
+++ gateway/src/restaurant/models/update-result.model.ts
@@ -0,0 +1,10 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class UpdateResult {
+    @Field((type) => Int)
+    affected: number;
+
+    @Field(type => String)
+    raw: any;
+}

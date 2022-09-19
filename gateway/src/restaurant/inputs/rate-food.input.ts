15f6d077c4591b581df5d02d23a9aa110ccd9260
SobhanKiani <45046992+SobhanKiani@users.noreply.github.com>
2022-09-20 00:16:45 +0430

    SFC-22 Rate Food

---
+++ gateway/src/restaurant/inputs/rate-food.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType, Int } from "@nestjs/graphql";
+import { IsNumber, IsString, Max, Min } from "class-validator";
+
+@InputType()
+export class RateFoodInput {
+    @Field((type) => Int)
+    @IsNumber()
+    @Min(1)
+    @Max(5)
+    rateNumber: number;
+
+    @Field((type) => Int)
+    @IsNumber()
+    foodId: number;
+}

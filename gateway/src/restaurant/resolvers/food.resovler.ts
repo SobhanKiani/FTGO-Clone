97436e82b19a2f31409c980f1b6d49c8fee2a558
SobhanKiani <45046992+SobhanKiani@users.noreply.github.com>
2022-09-20 00:48:40 +0430

    SFC-46 get food

--- gateway/src/restaurant/resolvers/food.resovler.ts
+++ gateway/src/restaurant/resolvers/food.resovler.ts
@@ -1,18 +1,21 @@
 import { HttpException, HttpStatus, Inject, UseGuards } from "@nestjs/common";
-import { Args, Int, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Int, Mutation, Resolver, Query } from "@nestjs/graphql";
 import { ClientProxy } from "@nestjs/microservices";
-import { firstValueFrom } from "rxjs";
+import { filter, firstValueFrom } from "rxjs";
 import { GetUser } from "src/authentication/decorators/get-user-from-request.decorator";
 import { IsPrivate } from "src/authentication/decorators/is-private.decorator";
 import { Roles } from "src/authentication/decorators/roles.decorator";
 import { Role } from "src/authentication/enums/roles.enum";
 import { RolesGuard } from "src/authentication/guards/roles.guard";
 import { User } from "src/authentication/models/user.model";
+import { FilterFoodQuery } from "../args/food-filter.args";
 import { CreateFoodInput } from "../inputs/create-food.input";
 import { RateFoodInput } from "../inputs/rate-food.input";
 import { UpdateFoodInput } from "../inputs/update-food.input";
 import { ICreateFoodResponse } from "../interfaces/create-food.interface";
 import { IDeleteFoodResponse } from "../interfaces/delete-food-response.interface";
+import { IGetFoodByIdResponse } from "../interfaces/get-food-by-id-reponse.interface";
+import { IGetFoodList } from "../interfaces/get-food-list-response.interface";
 import { IRate } from "../interfaces/rate-response.interface";
 import { IUpdateFoodResponse } from "../interfaces/update-food-response.interface";
 import { Food } from "../models/food.model";
@@ -96,6 +99,30 @@ export class FoodResovler {
             requestorId: user.id
         }
         const result = await firstValueFrom(this.restaurantClient.send<IRate>({ cmd: "rate_food" }, data));
+        if (result.status !== HttpStatus.OK) {
+            throw new HttpException({ message: result.message, errors: result.errors }, result.status);
+        }
+        return result.data;
+    }
+
+    @Query((returns) => Food)
+    async getFoodById(
+        @Args('id', { type: () => Int }) id: number
+    ) {
+        const params = { foodId: id }
+        const result = await firstValueFrom(this.restaurantClient.send<IGetFoodByIdResponse>({ cmd: "get_food_by_id" }, params));
+        if (result.status !== HttpStatus.OK) {
+            throw new HttpException({ message: result.message, errors: result.errors }, result.status);
+        }
+        return result.data;
+    }
+
+    @Query((returns) => [Food])
+    async getFoodList(
+        @Args({ type: () => FilterFoodQuery }) filter: FilterFoodQuery
+    ) {
+
+        const result = await firstValueFrom(this.restaurantClient.send<IGetFoodList>({ cmd: "get_food_list" }, filter));
         if (result.status !== HttpStatus.OK) {
             throw new HttpException({ message: result.message, errors: result.errors }, result.status);
         }

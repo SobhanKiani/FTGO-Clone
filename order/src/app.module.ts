2a2ffb9f93d22d3f314347ea3e10b3b120bc58db
SobhanKiani <45046992+SobhanKiani@users.noreply.github.com>
2022-10-06 00:24:51 +0330

    order microservice removed

--- order/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './services/prisma-service/prisma-service.service';
-
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [PrismaService],
-})
-export class AppModule { }
